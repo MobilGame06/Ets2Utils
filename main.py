c8e21b86e1412c4cf37db34456c60aecb3756cdc
Mobil Game 06 <nico.schnass@gmail.com>
2023-07-07 22:05:11 +0200

    more settings

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import customtkinter
 from CTkMessagebox import CTkMessagebox
+from CTkToolTip import *
 import antiAfk
 import threading
 from utils import settings_utils
@@ -18,10 +19,18 @@ def start_antiAFK():
         antiAfk.is_active = False
         antiAfkButton.configure(text="Activate Anti AFK", fg_color="#16a085", hover_color="#1abc9c")
 
+
+def show_tooltip(element, text):
+    tooltip = CTkToolTip(element, text)
+    tooltip.show()
+
 def save_antiAFK():
     settings_utils.update_entry(settings_file, "text", antiAfkTextbox.get('1.0', '6.0').replace('\n', ' '))
     CTkMessagebox(title="Info", message="Text Saved")
 
+def save_settings():
+    settings_utils.update_entry(settings_file, "telemetryIP", telemetryUrlTextbox.get('1.0', '6.0').replace('\n', ''))
+    CTkMessagebox(title="Info", message="Settings Saved")
 
 
 app = customtkinter.CTk()
@@ -56,7 +65,15 @@ antiAfkButton.grid(row=1, column=0, columnspan=2, padx=10, pady=(0, 10))
 #Settings TAB
 SettingsTab = tabview.tab("Settings")
 
+telemetryUrlLabel = customtkinter.CTkLabel(master=SettingsTab, text="Telemetry URL:")
+telemetryUrlLabel.grid(row=0, column=0, padx=10, pady=(20, 0))
+
 telemetryUrlTextbox = customtkinter.CTkTextbox(master=SettingsTab, width=400, height=10)
-telemetryUrlTextbox.grid(row=0, column=0, padx=10, pady=(20, 10))
+telemetryUrlTextbox.grid(row=1, column=0, padx=10, pady=10)
 telemetryUrlTextbox.insert("0.0", settings_utils.read_entry(settings_file, "telemetryIP"))
+telemetryUrlTextbox.bind("<Enter>", show_tooltip(telemetryUrlTextbox, "Format: http://localhost:25555"))
+
+settingsSaveButton = customtkinter.CTkButton(master=SettingsTab, text="Save", fg_color="#16a085", text_color="#2c3e50",hover_color="black" ,command=save_settings, width=50)
+settingsSaveButton.place(x=460, y=450)
+
 app.mainloop()
